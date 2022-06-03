d37d128c1dfbf1e503dedaa29d98e7426db21cad
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-06-03 18:53:09 +0500

    hardhat-fund-me

---
+++ deploy/99-deploy-storage-fun.js
@@ -0,0 +1,30 @@
+const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("----------------------------------------------------")
+    log("Deploying FunWithStorage and waiting for confirmations...")
+    const funWithStorage = await deploy("FunWithStorage", {
+        from: deployer,
+        args: [],
+        log: true,
+        // we need to wait if on a live network so we can verify properly
+        waitConfirmations: network.config.blockConfirmation || 1,
+    })
+
+    log("Logging storage...")
+    for (let i = 0; i < 10; i++) {
+        log(
+            `Location ${i}: ${await ethers.provider.getStorageAt(
+                funWithStorage.address,
+                i
+            )}`
+        )
+    }
+}
+
+module.exports.tags = ["storage"]
